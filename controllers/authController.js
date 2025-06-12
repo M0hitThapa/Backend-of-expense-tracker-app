66f675ae2e9633254e7824c72783308cc3ee3d2e
Mohit Thapa <thapamohit2058@gmail.com>
2025-06-12 23:41:03 +0545

    get user details

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,4 +60,14 @@ exports.loginUser = async (req, res) => {
     }
 };
 
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await user.findById(req.user.id).select("-password");
+    if(!user) {
+        return res.status(404).json({message:"User not found"});
+    }
+    res.status(200).json(user)
+} catch (err) {
+    res.status(500).json({message:"user not found", error:err.message})
+}
+};
