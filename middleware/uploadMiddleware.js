df81267a42f3a69e886982d97bfb0b487f7748d3
Mohit Thapa <thapamohit2058@gmail.com>
2025-06-16 23:43:45 +0545

    solve file upload error

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
 });
 
 
-const fileFilter = (req, res, cb) => {
+const fileFilter = (req, file, cb) => {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg']
     if (allowedTypes.includes(file.mimetype)) {
         cb(null, true);
