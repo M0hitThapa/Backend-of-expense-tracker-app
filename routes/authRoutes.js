f84f912b3e4a892a30c937c003de77f66152a5f9
Mohit Thapa <thapamohit2058@gmail.com>
2025-06-17 23:21:26 +0545

    upload image works

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,9 +20,7 @@ router.post("/login", loginUser)
         return res.status(400).json({message:"No file uplaoded"});
 
     }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename}
- }`;
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
 res.status(200).json({ imageUrl})
  })
 
