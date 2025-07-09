c36be65698e223e739d4f7c9c2e9ad038511a58c
Mohit Thapa <thapamohit2058@gmail.com>
2025-07-09 17:04:38 +0545

    add routes for income

---
+++ controllers/incomeController.js
@@ -0,0 +1,8 @@
+exports.addIncome = async (req, res) => {}
+
+
+exports.getAllIncome = async (req, res) => {}
+
+exports.deleteIncome = async (req, res) => {}
+
+exports.downloadIncomeExcel = async (req, res) => {}
